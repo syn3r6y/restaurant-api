70c19a84a39aaeac2446673eab165b65f30fc96d
syn3r6y <syn3rgy@live.ca>
2017-01-18 13:06:29 -0700

    adds controller and model structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import config from '../config';
 import middleware from '../middleware';
 import initializeDb from '../db';
+import restaurant from '../controller/restaurant';
 
 let router = express();
 
@@ -9,7 +10,9 @@ let router = express();
 initializeDb((db) => {
     // internal middleware
     router.use(middleware({ config, db }));
+    
     // api routes v1
+    router.use('/restaurant', restaurant({ config, db }));
 });
 
 export default router;
