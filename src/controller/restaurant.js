0f2992bca5cf2c2fa6cf7503d296d5f8a322bb0b
syn3r6y <syn3rgy@live.ca>
2017-01-18 15:27:39 -0700

    adds CRUD API methods

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -6,6 +6,8 @@ export default ({ config, db }) => {
     let api = Router();
 
     // v1/restaurant/add
+    
+    // POST method
     api.post('/add', (req, res) => {
         let newRest = new Restaurant();
         newRest.name = req.body.name;
@@ -15,6 +17,62 @@ export default ({ config, db }) => {
                 res.send(err);
             } else {
                 res.json({ message: 'Restaurant saved successfully.' });
+            }
+        });
+    });
+
+    // v1/restaurant/
+
+    // GET method
+    api.get('/', (req,res) => {
+        Restaurant.find({}, (err, restaurants) => {
+            if(err){
+                res.send(err);
+            } else{
+                res.json(restaurants);
+            }
+        });
+    });
+
+    // v1/restaurant/:id
+
+    // GET method returning single object
+    api.get('/:id', (req,res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if(err){
+                res.send(err);
+            } else{
+                res.json(restaurant);
+            }
+        });
+    });
+
+    // PUT method to update
+    api.put('/:id', (req,res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if(err){
+                res.send(err);
+            } else{
+                restaurant.name = req.body.name;
+                restaurant.save(err => {
+                    if(err){
+                        res.send(err);
+                    } else{
+                        res.json({ message: "Restaurant info udpated." });
+                    }
+                });
+            }
+        });
+    });
+
+    api.delete('/:id', (req,res) => {
+        Restaurant.remove({
+            _id: req.params.id
+        }, (err, restaurant) => {
+            if(err){
+                res.send(err);
+            } else {
+                res.json({ message: "Restaurant successfully removed." });
             }
         });
     });
